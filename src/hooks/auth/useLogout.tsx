a784dfc3648e438cc51da6baae841e52bddf109c
01011001andra <yandrayt1@gmail.com>
2025-08-31 15:24:24 +0700

    update

--- src/hooks/auth/useLogout.tsx
+++ src/hooks/auth/useLogout.tsx
@@ -1,21 +1,26 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
-import api from "../../api";
+import useRouter from "../apps/useRouter";
+import { useUserStore } from "@/stores/user";
+// import api from "../../api";
 
 // --- MUTATION: Logout ---
 export function useLogoutMutation() {
   const qc = useQueryClient();
+  const router = useRouter();
+  const { setUser } = useUserStore();
 
   return useMutation({
     mutationFn: async () => {
       try {
-        await api.post("/auth/logout");
+        // await api.post("/auth/logout");
+        setUser(null);
+        localStorage.removeItem("token");
       } catch {
         // abaikan error logout server (opsional)
+      } finally {
+        router.push("/login");
+        qc.clear();
       }
-    },
-    onSuccess: () => {
-      // Hapus semua cache query
-      qc.clear();
     },
   });
 }
